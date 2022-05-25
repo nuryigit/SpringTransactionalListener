514abfa527bb4e8bfd883f80f4180b6bc0e70a3a
fatma.yigit <f.nuryigit1@gmail.com>
2022-05-25 23:31:52 +0300

    first commit

---
+++ src/main/java/com/ny/listener/SpringTransactionalListenersApplication.java
@@ -0,0 +1,13 @@
+package com.ny.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionalListenersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionalListenersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ny/listener/SpringTransactionalListenersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ny.listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionalListenersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
